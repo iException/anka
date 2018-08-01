75d04dcc47a3058d86a460da68399758d86b402e
liren <liren@baixing.com>
2018-08-01 14:15:50 +0800

    update init script

--- bin/init.js
+++ bin/init.js
@@ -5,24 +5,46 @@ const inquirer = require('inquirer')
 const CONFIG = require('../config')
 const download = require('download-git-repo')
 
+/**
+ * Make directory depending on whether it is related to current path.
+ */
+function mkdir(isRelative) {
+    let defaultPath = isRelative ? 'wxApp' : `${process.cwd()}/wxApp`
+    inquirer.prompt({
+        name: 'dirName',
+        type: 'input',
+        message: `请输入小程序目录名称:(${defaultPath})`,
+        default: defaultPath
+    }).then((res) => {
+        let path = isRelative ? './' + res.dirName : res.dirName
+        return new Promise((resolve, reject) => {
+            if (fs.existsSync(path)) {
+                reject('该目录已存在')
+            } else {
+                resolve(path)
+            }
+        })
+    }).then(res => {
+        return new Promise((resolve, reject) => {
+            log.base('downloading....')
+            download(CONFIG.scaffold, res, { clone: true }, function(err) {
+                err ? reject(err) : resolve()
+            })
+        })
+    }).then(res => {
+        log.base('directory created successfully!')  
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
 module.exports = function () {
     inquirer.prompt({
         name: 'currentPath',
         type: 'confirm',
         message: `是否在当前目录创建小程序？(${process.cwd()})`
-    }).then((...currentPath) => {
-        if (currentPath) {
-            return new Promise((resolve, reject) => {
-                log.base('downloading....')
-                download(CONFIG.scaffold, './wxApp', { clone: true }, function(err) {
-                    err ? reject(err) : resolve()
-                })
-            })
-        } else {
-            reject(new Error('已取消'))
-        }
-    }).then(res => {
-        log.success('')
+    }).then((res) => {
+        mkdir(res.currentPath)
     }).catch(err => {
         console.log(err)
     })
