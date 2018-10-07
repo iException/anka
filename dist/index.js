83da40fb81cfda9876f1cee38ade41146d87ec70
HeZaichang.WeRDyin <heyin223@163.com>
2018-10-07 20:02:41 +0800

    build

--- dist/index.js
+++ dist/index.js
@@ -977,7 +977,7 @@ var removeComponent = {
 var commands = [init, dev, build, genPage$1, genComponent$1, addComponent, removeComponent];
 
 var name = "@anka-dev/cli";
-var version = "0.2.6";
+var version = "0.2.7";
 var description = "WeChat miniprogram helper";
 var bin = {
 	anka: "dist/index.js"
