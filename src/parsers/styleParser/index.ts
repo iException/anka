5c57109d5a975d7c6c7b1ad2995afce380379574
HeZaichang.WeRDyin <heyin223@163.com>
2018-12-11 00:37:23 +0800

    Remove cssnano for optimization

--- src/parsers/styleParser/index.ts
+++ src/parsers/styleParser/index.ts
@@ -1,4 +1,5 @@
 import * as Postcss from 'postcss'
+import logger from '../../utils/logger'
 import postcssrc from 'postcss-load-config'
 
 import {
@@ -11,22 +12,38 @@ import * as PostCSS from 'postcss'
 
 const postcss = require('postcss')
 const postcssConfig: any = {}
+const internalPlugins: Array<PostCSS.AcceptedPlugin> = []
+const tasks: any[] = []
+
+// TODO: Add new hook: preset
 
 /**
  * Style file parser.
  * @for .wxss .css => .wxss
  */
 export default <Parser>function (this: ParserInjection, file: File, compilation: Compilation, cb: Function): void {
-    const internalPlugins: Array<PostCSS.AcceptedPlugin> = []
+    if (postcssConfig.plugins) {
+        exec(postcssConfig, file, cb)
+    } else {
+        tasks.push(() => {
+            exec(postcssConfig, file, cb)
+        })
+    }
+}
 
-    genPostcssConfig().then((config: any) => {
-        file.convertContentToString()
+genPostcssConfig().then((config: any) => {
+    tasks.forEach((task: Function) => task())
+}).catch((err: Error) => {
+    logger.error('loadConfig', err.message, err)
+})
 
-        return postcss(config.plugins.concat(internalPlugins)).process(file.content, {
-            ...config.options,
-            from: file.sourceFile
-        } as Postcss.ProcessOptions)
-    }).then((root: Postcss.Result) => {
+
+function exec (config: any, file: File, cb: Function) {
+    file.convertContentToString()
+    postcss(config.plugins.concat(internalPlugins)).process(file.content, {
+        ...config.options,
+        from: file.sourceFile
+    } as Postcss.ProcessOptions).then((root: Postcss.Result) => {
         file.content = root.css
         file.ast = root.root.toResult()
         file.updateExt('.wxss')
@@ -34,8 +51,7 @@ export default <Parser>function (this: ParserInjection, file: File, compilation:
     })
 }
 
-
-function genPostcssConfig () {
+function genPostcssConfig (tasks: Function[] = []) {
     return postcssConfig.plugins ? Promise.resolve(postcssConfig) : postcssrc({}).then((config: any) => {
         return Promise.resolve(Object.assign(postcssConfig, config))
     })
